b94bc28e58d3d95762c8e433842897dafb045b10
talchyn <talchynerkinbekova@gmail.com>
2022-09-28 14:20:06 +0600

    Initial commit

---
+++ app/src/main/java/com/example/android2/ui/fragments/onboard/OnBoardFragment.kt
@@ -0,0 +1,33 @@
+package com.example.android2.ui.fragments.onboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.android2.R
+import com.example.android2.databinding.FragmentOnBoardBinding
+import com.example.android2.ui.adapters.OnBoardAdapter
+
+class OnBoardFragment : Fragment() {
+
+    private lateinit var binding: FragmentOnBoardBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentOnBoardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewPager.adapter = OnBoardAdapter(this@OnBoardFragment)
+    }
+
+
+
+
+}
